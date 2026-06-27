2e6ab0c9f1c93e52874abf5d779279cba6c40b53
Logical <izzy.asad@gmail.com>
2026-06-27 18:43:41 -0400

    Added a line number as well as a column number.
    That's not all, ANSI color escape codes are now are in #define so that we can reuse the color codes.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+// Define ansi escape codes so that you can use it to change the color of the text
+#define NUM_LINE_OR_COL_COLOR "\e[38;2;255;255;0m"
+#define RESET_COLOR "\e[0m"
+#define NULL_BYTE_COLOR "\e[38;2;255;0;0m"
+#define END_OF_FILE_COLOR "\e[38;2;100;100;100m"
 
 template <typename T>
-std::string VarToHex(T var) {
+std::string ToHex(T var) {
 	std::string hex_code = std::format("{:X}", var);
 
 	// If there is one hex character add a 0 before that hex character
@@ -40,24 +45,32 @@ int main(int argc, char** argv) {
 	// If there is 1 argument then attempt to open the file
 	if (argc == 2) {
 		std::string file_contents = ReadFile(argv[1]);
-		int line = (line_width*lines_visible);
-		for (int i = (scroll-1)*line; i < scroll*line; i++) {
+		int lines_length = (line_width*lines_visible);
+		int line_number = 0;
+		// There is a "00: " so add a bunch of spaces to make it so that it is aligned with the hex code below
+		std::cout << "    ";
+		for (int i = 0; i < line_width; i++) {
+			std::cout << NUM_LINE_OR_COL_COLOR << ToHex<int>(i) << RESET_COLOR << " ";
+		}
+		for (int i = (scroll-1)*lines_length; i < scroll*lines_length; i++) {
 			if (i%line_width == 0) {
-				std::cout << std::endl;
+				std::cout << std::endl << NUM_LINE_OR_COL_COLOR << ToHex<int>(line_number) << ": " << RESET_COLOR;
+				line_number++;
+
 			}
 			if (file_contents.size() > i) {
 				char character = file_contents.c_str()[i];
-				std::string hex_byte = VarToHex<char>(character);
+				std::string hex_byte = ToHex<char>(character);
 				// If it is a null character, show it as red
 				if (character == '\x00') {
-					std::cout << "\e[38;2;255;0;0m";
+					std::cout << NULL_BYTE_COLOR;
 				}
 
 
-				std::cout << hex_byte << "\e[0m ";
+				std::cout << hex_byte << RESET_COLOR << " ";
 			} else {
 				// Make it show as gray
-				std::cout << "\e[38;2;100;100;100m00\e[0m" << " ";
+				std::cout << END_OF_FILE_COLOR << "00" << RESET_COLOR << " ";
 			}
 		}
 
